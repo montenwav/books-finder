09cbbec5200eeb4bcb6e6e7fe3b29c7724c113b2
Vlad <157234524+montenwav@users.noreply.github.com>
2024-07-02 12:30:49 +0300

    Reapply "search toggle bug"
    This reverts commit 5df560f9f8a9d7c0c12dfd12be1556c4d7cc9ef5.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,25 +1,47 @@
 import Filters from '../components/Filters';
 import {Sort} from '../components/Sort';
 import Books from '../components/Books';
+import { useEffect, useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks/reducerHooks';
+import { setIsSearching } from '../slices/SearchSlice';
 
 export default function Main() {
   return (
-    <>
-      <MainComponents>
+    <MainComponents>
+      <div className="inner_main">
         <Sort />
         <div className="books_and_filters">
           <Filters />
           <Books />
         </div>
-      </MainComponents>
-    </>
+      </div>
+    </MainComponents>
   );
 }
 
 const MainComponents = ({ children }: { children: React.ReactNode }) => {
+  const ref = useRef<HTMLDivElement  | null>(null);
+  const dispatch = useAppDispatch();
+  const { isSearching } = useAppSelector((state) => state.search);
+
+  const handleClear = () => {
+    if(ref.current) {
+      dispatch(setIsSearching(false));
+    }
+  };
+
+  useEffect(() => {
+    if (isSearching) {
+      document.addEventListener('click', handleClear);
+      return () => document.removeEventListener('click', handleClear);
+    }
+  }, [isSearching]);
+
   return (
-    <main>
-      <div className="inner_main">{children}</div>
-    </main>
+    <>
+      <main>
+        <div ref={ref} className="outer_main">{children}</div>
+      </main>
+    </>
   );
 };
